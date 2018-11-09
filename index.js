d2053ef0c390c0a23584d4a88b40b68eaa7496a1
Bharath <bharathanand000@gmail.com>
2018-11-09 14:16:59 +0530

    mongoose part 2

--- index.js
+++ index.js
@@ -17,11 +17,25 @@ connect.then((db) => {
     
     .then((dish)=> {
         console.log(dish);
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set : {description : "Updated test"}
+        },{
+         new : true
+    }).exec();
     })
-    .then((dishes)=> {
-        console.log(dishes);
+    .then((dish)=> {
+        console.log(dish);
 
+        dish.comments.push({
+            rating :5,
+            comment: 'Nice and yummy!',
+            author :'yolo'
+        });
+        return dish.save();
+    })
+    .then((dish) => {
+        
+        console.log(dish);
         return Dishes.remove({});
     })
     .then(() => {
